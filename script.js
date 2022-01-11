859f5f215c783c16704937bef5a6d7fb54abd0f6
Tam <62277018+Tam8thang@users.noreply.github.com>
2022-01-11 20:15:53 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if(n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"
+  }
+  slides[slideIndex-1].style.display = "block";
+
+}
+
+function boot(n) {
+  document.getElementById('sumbnails').style.display = 'none';
+  document.getElementById('views').style.display = 'block';
+  currentSlide(n)
+}
+
+function backToTop() {
+  document.getElementById('sumbnails').style.display = 'flex';
+  document.getElementById('views').style.display = 'none';
+}
